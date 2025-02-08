99a24e5aa065896610427eb87c8e31072eab196f
lunox94 <gmrlunox94@gmail.com>
2025-02-08 17:40:31 +0100

    Refactor main.rb to encapsulate game options in RubyChess class and improve user interaction flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,32 +1,56 @@
 # frozen_string_literal: true
 
-EXIT_OPTION = 4
+# The RubyChess class is used to show the main menu of the game and handle the options selected by the user.
+class RubyChess
+  NEW_GAME_OPTION = 1
+  RESUME_GAME_OPTION = 2
+  ANALYSIS_BOARD_OPTION = 3
+  EXIT_OPTION = 4
 
-def show_options
-  puts 'Welcome to RubyChess'
-
-  loop do
-    puts 'Please choose an option to start:'
-
-    print_options
-
-    option = gets.chomp.to_i
-
-    break if exit_selected?(option)
+  def show_options
+    puts 'Welcome to RubyChess'
+    main_loop
+    puts 'See you soon, thanks for playing RubyChess.'
   end
 
-  puts 'See you soon, thanks for playing RubyChess.'
+  def main_loop
+    loop do
+      puts 'Please choose an option to start:'
+      print_options
+      option = gets.chomp.to_i
+      handle_option(option)
+      break if exit_selected?(option)
+    end
+  end
+
+  def print_options
+    puts "#{NEW_GAME_OPTION}. New game"
+    puts "#{RESUME_GAME_OPTION}. Resume game"
+    puts "#{ANALYSIS_BOARD_OPTION}. Analysis board"
+    puts "#{EXIT_OPTION}. Exit"
+  end
+
+  def handle_option(option)
+    case option
+    when NEW_GAME_OPTION
+      puts 'Starting a new game...'
+      # ...handle new game logic...
+    when RESUME_GAME_OPTION
+      puts 'Resuming game...'
+      # ...handle resume game logic...
+    when ANALYSIS_BOARD_OPTION
+      puts 'Opening analysis board...'
+      # ...handle analysis board logic...
+    when EXIT_OPTION
+      puts 'Exiting...'
+    else
+      puts 'Invalid option, please try again.'
+    end
+  end
+
+  def exit_selected?(option)
+    option == EXIT_OPTION
+  end
 end
 
-def print_options
-  puts '1. New game'
-  puts '2. Resume game'
-  puts '3. Analysis board'
-  puts "#{EXIT_OPTION}. Exit"
-end
-
-def exit_selected?(option)
-  option == EXIT_OPTION
-end
-
-show_options
+RubyChess.new.show_options
