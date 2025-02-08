9a89dd52d0e7061d99fc7ceeb046fb548bf97234
lunox94 <gmrlunox94@gmail.com>
2025-02-08 17:42:01 +0100

    Refactor main.rb to improve option handling and encapsulate exit logic

--- bin/main.rb
+++ bin/main.rb
@@ -13,21 +13,10 @@ class RubyChess
     puts 'See you soon, thanks for playing RubyChess.'
   end
 
-  def main_loop
-    loop do
-      puts 'Please choose an option to start:'
-      print_options
-      option = gets.chomp.to_i
-      handle_option(option)
-      break if exit_selected?(option)
-    end
-  end
+  private
 
-  def print_options
-    puts "#{NEW_GAME_OPTION}. New game"
-    puts "#{RESUME_GAME_OPTION}. Resume game"
-    puts "#{ANALYSIS_BOARD_OPTION}. Analysis board"
-    puts "#{EXIT_OPTION}. Exit"
+  def exit_selected?(option)
+    option == EXIT_OPTION
   end
 
   def handle_option(option)
@@ -48,8 +37,21 @@ class RubyChess
     end
   end
 
-  def exit_selected?(option)
-    option == EXIT_OPTION
+  def main_loop
+    loop do
+      puts 'Please choose an option to start:'
+      print_options
+      option = gets.chomp.to_i
+      handle_option(option)
+      break if exit_selected?(option)
+    end
+  end
+
+  def print_options
+    puts "#{NEW_GAME_OPTION}. New game"
+    puts "#{RESUME_GAME_OPTION}. Resume game"
+    puts "#{ANALYSIS_BOARD_OPTION}. Analysis board"
+    puts "#{EXIT_OPTION}. Exit"
   end
 end
 
