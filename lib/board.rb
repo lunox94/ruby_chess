6c0c2247921ad657c44db76aa34b64910ad043c3
lunox94 <gmrlunox94@gmail.com>
2025-02-10 18:00:14 +0100

    Add piece management to Board class with position validation

--- lib/board.rb
+++ lib/board.rb
@@ -4,10 +4,25 @@
 class Board
   NUMBER_OF_ROWS = 8
   NUMBER_OF_COLUMNS = 8
+  INVALID_POSITION_ERROR = 'Invalid position'
 
   attr_reader :grid
 
   def initialize
     @grid = Array.new(NUMBER_OF_ROWS) { Array.new(NUMBER_OF_COLUMNS) }
+  end
+
+  def add_piece(piece, position)
+    raise INVALID_POSITION_ERROR unless valid_position?(position)
+
+    row, col = position
+    @grid[row][col] = piece
+  end
+
+  private
+
+  def valid_position?(position)
+    row, col = position
+    row.between?(0, NUMBER_OF_ROWS - 1) && col.between?(0, NUMBER_OF_COLUMNS - 1)
   end
 end
