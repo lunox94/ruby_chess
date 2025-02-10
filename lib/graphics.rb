145ecc7c631759fab5a6185e9a48d9c935876f84
lunox94 <gmrlunox94@gmail.com>
2025-02-10 18:01:45 +0100

    Refactor Graphics class to improve board display logic and symbol representation

--- lib/graphics.rb
+++ lib/graphics.rb
@@ -5,10 +5,10 @@ class Graphics
   def self.display(board)
     board.each_with_index do |row, row_index|
       row_display = row.map.with_index do |square, col_index|
-        if (row_index + col_index).odd?
-          square.nil? ? "\u25A0" : square # Black square
+        if square
+          square.symbol
         else
-          square.nil? ? "\u25A1" : square # White square
+          (row_index + col_index).odd? ? "\u25A0" : "\u25A1"
         end
       end
       puts row_display.join(' ')
