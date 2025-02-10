d1347d4de95ef455786a86ae8646fa64fee6bfb9
lunox94 <gmrlunox94@gmail.com>
2025-02-10 18:01:52 +0100

    Add Piece module to define color and symbol for game pieces

---
+++ lib/piece.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# This module is used to define the color and symbol of a piece.
+module Piece
+  BLACK = :black
+  WHITE = :white
+
+  attr_reader :color, :symbol
+end
