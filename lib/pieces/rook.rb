76d723570e3a587aeb4b7b04d7b468a60ae2de9c
lunox94 <gmrlunox94@gmail.com>
2025-02-11 18:29:23 +0100

    Implement available_moves method for Rook with directional movement logic

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -6,12 +6,55 @@ require_relative '../piece'
 class Rook
   include Piece
 
+  UP = [1, 0].freeze
+  DOWN = [-1, 0].freeze
+  LEFT = [0, -1].freeze
+  RIGHT = [0, 1].freeze
+
+  MOVES = [UP, DOWN, LEFT, RIGHT].freeze
+
+  VERTICAL = [UP, DOWN].freeze
+  HORIZONTAL = [LEFT, RIGHT].freeze
+
   def initialize(color)
     @color = color
     @symbol = color == :white ? "\u2656" : "\u265C"
   end
 
-  def available_moves
-    # Implement the logic for available moves for a Rook
+  def available_moves(board, position)
+    super
+
+    moves = []
+
+    MOVES.each do |move|
+      moves.concat(available_moves_in_direction(board, position, move))
+    end
+
+    moves
+  end
+
+  private
+
+  def available_moves_in_direction(board, position, direction)
+    moves = []
+    current_position = position
+
+    loop do
+      current_position = advance_position(current_position, direction)
+
+      break unless Board.valid_position?(current_position)
+
+      piece = board.piece_at(current_position)
+
+      moves << current_position if piece.nil? || piece.color != color
+
+      break if piece
+    end
+
+    moves
+  end
+
+  def advance_position(position, direction)
+    [position[0] + direction[0], position[1] + direction[1]]
   end
 end
