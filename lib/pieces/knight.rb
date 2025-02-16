ca3320881cdf930dc142303a6e73d663c423da68
lunox94 <gmrlunox94@gmail.com>
2025-02-16 21:47:47 +0100

    Implement available_moves method for Knight class with movement logic

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -6,12 +6,35 @@ require_relative '../piece'
 class Knight
   include Piece
 
+  MOVES = [
+    [-2, -1], [-2, 1], [2, -1], [2, 1],
+    [-1, -2], [-1, 2], [1, -2], [1, 2]
+  ].freeze
+
   def initialize(color)
     @color = color
     @symbol = color == :white ? "\u2658" : "\u265E"
   end
 
-  def available_moves
-    # Implement the logic for available moves for a Knight
+  def available_moves(board, position)
+    moves = []
+
+    MOVES.each do |move|
+      next_position = advance_position(position, move)
+
+      next unless Board.valid_position?(next_position)
+
+      piece = board.piece_at(next_position)
+
+      moves << next_position if piece.nil? || piece.color != @color
+    end
+
+    moves
+  end
+
+  private
+
+  def advance_position(position, direction)
+    [position[0] + direction[0], position[1] + direction[1]]
   end
 end
