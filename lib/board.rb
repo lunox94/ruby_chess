2f4e06bf81fadc5d52329b625e2853e8ef8eba25
lunox94 <gmrlunox94@gmail.com>
2025-02-19 12:24:48 +0100

    Implement in_check? and square_controlled? methods in Board class

--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,7 @@
 class Board
   NUMBER_OF_ROWS = 8
   NUMBER_OF_COLUMNS = 8
+  NO_KING_FOUND_ERROR = 'No king found'
   INVALID_POSITION_ERROR = 'Invalid position'
 
   attr_reader :grid
@@ -19,11 +20,47 @@ class Board
     @grid[row][col] = piece
   end
 
+  def in_check?(color)
+    king_position = nil
+
+    each_piece do |piece, position|
+      next unless piece.color == color && piece.is_a?(King)
+
+      king_position = position
+      break
+    end
+
+    raise NO_KING_FOUND_ERROR if king_position.nil?
+
+    square_controlled?(color == Piece::WHITE ? Piece::BLACK : Piece::WHITE, king_position)
+  end
+
   def piece_at(position)
     raise INVALID_POSITION_ERROR unless self.class.valid_position?(position)
 
     row, col = position
     @grid[row][col]
+  end
+
+  def square_controlled?(color, target)
+    raise INVALID_POSITION_ERROR unless self.class.valid_position?(target)
+
+    each_piece do |piece, position|
+      next if piece.color != color
+
+      return true if piece.available_moves(self, position).include?(target)
+    end
+
+    false
+  end
+
+  def each_piece
+    NUMBER_OF_ROWS.times do |row|
+      NUMBER_OF_COLUMNS.times do |col|
+        piece = @grid[row][col]
+        yield(piece, [row, col]) if piece
+      end
+    end
   end
 
   def self.valid_position?(position)
