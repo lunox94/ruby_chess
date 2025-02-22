ed9e07951c71d9ef7329950e319bc471994658db
lunox94 <gmrlunox94@gmail.com>
2025-02-22 22:57:53 +0100

    Enhance GameState class to include game status management and move validation methods

--- lib/game_state.rb
+++ lib/game_state.rb
@@ -2,7 +2,12 @@
 
 # This class represents the state of the game at a given moment.
 class GameState
-  attr_reader :active_color, :board
+  DRAW = :draw
+  WHITE_WON = :white_won
+  BLACK_WON = :black_won
+  IN_PROGRESS = :in_progress
+
+  attr_reader :active_color, :board, :status
 
   attr_accessor :en_passant_square, :halfmove_clock, :fullmove_number, :castling_availability
 
@@ -15,6 +20,56 @@ class GameState
     @en_passant_square = en_passant_square
     @halfmove_clock = halfmove_clock
     @fullmove_number = fullmove_number
+
+    recalculate_status
   end
   # rubocop:enable Metrics/ParameterLists
+
+  def recalculate_status
+    return @status = DRAW if draw?
+    return @status = WHITE_WON if checkmate?(Piece::BLACK)
+    return @status = BLACK_WON if checkmate?(Piece::WHITE)
+
+    @status = IN_PROGRESS
+  end
+
+  def valid_move?(from, to)
+    piece = @board.piece_at(from)
+
+    return false if piece.nil? || piece.color != @active_color
+
+    available_moves = piece.available_moves(@board, from)
+
+    available_moves.include?(to)
+  end
+
+  def invalid_move?(from, to)
+    !valid_move?(from, to)
+  end
+
+  def make_move(from, to)
+    @board.move_piece(from, to)
+
+    @halfmove_clock += 1
+
+    @fullmove_number += 1 if @active_color == Piece::BLACK
+
+    switch_active_color
+
+    recalculate_status
+  end
+
+  private
+
+  def draw?
+    false
+  end
+
+  def checkmate?(color)
+    false
+  end
+
+  def switch_active_color
+    @active_color = @active_color == Piece::WHITE ? Piece::BLACK : Piece::WHITE
+  end
 end
